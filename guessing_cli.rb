8fa8681fd4bf1f94b6055cba75275fcf06bdf13a
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-27 04:58:18 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,9 +6,12 @@ def run_guessing_game
  num = rand(1..10)
  if input.downcase == "exit"
    puts "Goodbye!"
+   break
    elsif input.to_i == num
    puts "You guessed the correct number!"
- else
+   elsif input.to_i != num
    puts "The computer guessed #{num}."
+ else 
+   puts "Invalid input."
  end
 end 
