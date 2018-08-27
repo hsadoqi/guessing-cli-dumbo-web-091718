b79b3f34d3a767976fcc148bea04b9f67c962fff
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-27 04:52:51 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,11 +1,12 @@
 # Code your solution here!
 
 def run_guessing_game
-  input = gets.chomp
   num = rand()
-  until 
+  until input == "exit"
+  input = gets.chomp
   if input == num 
     puts "You guessed the correct number!"
+  else
+    puts "The number guessed #{num}."
   end 
-  
 end 
